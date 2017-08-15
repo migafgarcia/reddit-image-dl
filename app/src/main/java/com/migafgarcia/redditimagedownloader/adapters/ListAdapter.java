086c024b0437f36d54d4bb5d9cc7fa235a4d419f
migafgarcia <migafgarcia@gmail.com>
2017-08-15 22:01:34 +0100

    Added new activity, changed style

--- app/src/main/java/com/migafgarcia/redditimagedownloader/adapters/ListAdapter.java
+++ app/src/main/java/com/migafgarcia/redditimagedownloader/adapters/ListAdapter.java
@@ -2,7 +2,9 @@ package com.migafgarcia.redditimagedownloader.adapters;
 
 import android.content.Context;
 import android.graphics.drawable.GradientDrawable;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
+import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,10 +14,14 @@ import com.migafgarcia.redditimagedownloader.R;
 import com.migafgarcia.redditimagedownloader.controllers.Controller;
 import com.migafgarcia.redditimagedownloader.data.Post;
 import com.migafgarcia.redditimagedownloader.data.RedditResponse;
+import com.migafgarcia.redditimagedownloader.data.Resolution;
 import com.migafgarcia.redditimagedownloader.services.RedditService;
 import com.squareup.picasso.Picasso;
 
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.ArrayList;
@@ -60,17 +66,37 @@ public class ListAdapter extends RecyclerView.Adapter<ListItemViewHolder> {
 
         Post post = posts.get(position);
 
-        if (position == getItemCount() - 1 && getItemCount() > 0) {
-            service.getListAfter(context.getString(R.string.multireddit), after).enqueue(new Controller(context, this));
-        }
+        if (position == getItemCount() - 1 && getItemCount() > 0)
+            service.getListAfter(context.getString(R.string.multireddit), after).
+                    enqueue(new Controller(context, this));
 
         holder.title.setText(post.getData().getTitle());
         holder.subreddit.setText(post.getData().getSubreddit());
         holder.user.setText(post.getData().getAuthor());
 
-        Picasso.with(context).load(post.getData().getThumbnail()).placeholder(R.color.cardview_dark_background).into(holder.preview);
+        if(post.getData().getPreview().getEnabled()) {
+            List<Resolution> resolutions = post.getData().getPreview().getImages().get(0).getResolutions();
 
+            int index = resolutionIndex(resolutions);
 
+            Log.d(TAG, Html.fromHtml(resolutions.get(resolutions.size() - 1).getUrl()).toString());
+            Picasso.with(context).
+                    load(Html.fromHtml(resolutions.get(resolutions.size() - 1).getUrl()).toString()).
+                    placeholder(R.color.cardview_dark_background).
+                    resize(resolutions.get(resolutions.size() - 1).getWidth(), resolutions.get(resolutions.size() - 1).getHeight()).
+                    into(holder.preview);
+
+        }
+        else
+            Picasso.with(context).
+                    load(post.getData().getThumbnail()).
+                    resize(post.getData().getThumbnailWidth(), post.getData().getThumbnailHeight()).
+                    placeholder(R.color.cardview_dark_background).
+                    into(holder.preview);
+    }
+
+    private int resolutionIndex(List<Resolution> resolutions) {
+        return 0;
     }
 
     @Override
